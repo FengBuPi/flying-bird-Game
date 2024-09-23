cd502e90afc764517e3b6bd123351ef74dbfa3ff
疯不皮 <2921744480@qq.com>
2024-09-23 16:08:20 +0800

    feat : 组件注入

--- src/Router/Router.ts
+++ src/Router/Router.ts
@@ -2,7 +2,6 @@ export interface RouterOption {
   name: string;
   url: string;
   template: HTMLTemplateElement;
-  otherTemplates: HTMLTemplateElement[];
 }
 
 export default class Router {
@@ -30,15 +29,34 @@ export default class Router {
     callback()
   }
 
+  // 添加背景
+  async injectBackground() {
+    const parser = new DOMParser(); // 解析html字符串为dom对象
+    const background = parser.parseFromString(await fetch('../../Components/Background/background.html').then(response => response.text()), 'text/html');
+    const styleLink = document.createElement('link');
+    styleLink.rel = 'stylesheet';
+    styleLink.href = '../../Components/Background/background.css';
+    customElements.define(
+      "back-ground",
+      class extends HTMLElement {
+        constructor() {
+          super();
+          const shadowRoot = this.attachShadow({ mode: "open" })
+          const template = (background.getElementById("background") as HTMLTemplateElement).content;
+          shadowRoot.appendChild(template.cloneNode(true));
+          // 将 link 中的css样式添加到 Shadow DOM
+          shadowRoot.appendChild(styleLink);
+        }
+      },
+    );
+  }
   render(page: RouterOption) {
     if (!page) return
     this.app.innerHTML = ''; // 移除旧的模板(移除旧的模板时,没有对所有dom元素监听解除,导致无法释放内存,是否有通用的解法获得当前模板所有的事件监听)
     const fragment = document.createDocumentFragment(); // 创建片段
     const clone = page.template.content.cloneNode(true); // 克隆模板
     fragment.appendChild(clone);
-    page.otherTemplates.forEach(template => {
-      fragment.appendChild(template.content.cloneNode(true)); // 挂载子模板
-    });
     this.app.appendChild(fragment);
+    this.injectBackground()
   }
 }
